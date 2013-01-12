3df94754b498f6d198d3f184edc4a7f916c697d0
Sébastien Drouyer <sdrdis@gmail.com>
2013-01-13 00:50:07 +0100

    date time application init

---
+++ README.md
@@ -0,0 +1,6 @@
+Datetime
+========
+
+This [novius-os](https://github.com/novius-os/novius-os) application allows you to add datetime and datetime range rendrer.
+
+[Icon](http://www.iconfinder.com/icondetails/2013/128/clock_time_icon) from [Alexandre Moore](http://sa-ki.deviantart.com/).
---
+++ config/metadata.config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+return array(
+    'name'    => 'DateTime',
+    'version' => '0.1',
+    'provider' => array(
+        'name' => 'Sdrdis',
+    ),
+    'namespace' => 'Sdrdis\Datetime',
+    'permission' => array(
+    ),
+    'icons' => array(
+        16 => 'static/apps/sdrdis_datetime/img/datetime-16.png',
+        32 => 'static/apps/sdrdis_datetime/img/datetime-32.png',
+        64 => 'static/apps/sdrdis_datetime/img/datetime-64.png',
+    ),
+);
